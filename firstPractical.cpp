7bb55c093ff2bd616be44a41cd0a4778d24a2ec8
JadeShopKeeper <soap4university@gmail.com>
2023-01-31 12:17:23 +0200

    1

--- firstPractical.cpp
+++ firstPractical.cpp
@@ -1,1 +1,27 @@
-int i;   
+#include <iostream>
+using namespace std;
+
+struct Date{
+   int year;
+};
+
+struct Auto{
+   int wheels;
+   float speed;
+   char color;
+   Date sozdanie;
+};
+
+int main(){
+   Auto Skoda;
+   Skoda.color = 'r';
+   Skoda.speed = 315.12;
+   Skoda.wheels = 4;
+   Skoda.sozdanie.year = 1999;
+   Auto Audi = {4,300.23, "b"};
+
+   cout << "Audi speed = "<< Audi.speed << endl;
+   cout << "Skoda year = "<< Skoda.sozdanie.year  << endl;
+    
+}
+
